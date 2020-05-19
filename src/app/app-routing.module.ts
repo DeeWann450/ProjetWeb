df506b3f47bb135e5cbee8d749c1b204a53c6497
DeeWann450 <gleviavant@yahoo.com>
2020-05-19 18:06:41 +0200

    Ajout d'une barre de navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BirthRennesComponent } from './birth-rennes/birth-rennes.component';
 
-
 const routes: Routes = [
-  { path: '', component: BirthRennesComponent}
+  { path: '', component: BirthRennesComponent},
 ];
 
 @NgModule({
