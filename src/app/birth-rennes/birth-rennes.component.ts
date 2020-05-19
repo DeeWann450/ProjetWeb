21583af6ffce9a0cc9e8f5f314aad3c3f06d9ab7
DeeWann450 <gleviavant@yahoo.com>
2020-05-19 14:53:43 +0200

    Suppression du service qui ne servait à rien, l'appel à l'API fonctionne

--- src/app/birth-rennes/birth-rennes.component.ts
+++ src/app/birth-rennes/birth-rennes.component.ts
@@ -1,4 +1,4 @@
-import { BirthService } from 'src/app/birth.service'
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,12 +10,16 @@ export class BirthRennesComponent implements OnInit {
 
   births;
 
-  constructor(
-    private birthService: BirthService,
-    ) { }
+  private apiURL = 'https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=naissances-a-rennes&q=&rows=10&sort=annee&facet=annee&facet=garcons'
+
+  constructor(private http: HttpClient) {    
+    this.http.get(this.apiURL).toPromise().then(data => {
+      console.log(data);
+      this.births = data;
+    })
+  }
 
   ngOnInit(): void {
-    this.births = this.birthService.getBirths();
   }
 
 }
